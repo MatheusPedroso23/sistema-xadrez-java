aff57d9ac97eecda225eb9fa669d89258a0cd6e6
Matheus Pedroso <pedroso.matheus2000@gmail.com>
2024-03-17 16:18:05 -0300

    moving pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.Rei;
 import chess.pieces.Torre;
 
@@ -24,6 +26,28 @@ public class ChessMatch {
 			
 		}
 		return mat;
+	}
+	
+	
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece)capturedPiece;
+	}
+	
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.PlacePiece(p, target);
+		return capturedPiece;
+	}
+	
+	private void validateSourcePosition(Position position) {
+		if(!board.thereisAPiece(position)) {
+			throw new ChessException("nao tem peca na posicao");
+		}
 	}
 	
 	private void placeNewPiece(char column , int row, ChessPiece piece) {
