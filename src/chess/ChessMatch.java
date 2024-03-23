c781fd3a9771e5850c24c6e9271d10558b9a6ef4
Matheus Pedroso <pedroso.matheus2000@gmail.com>
2024-03-22 21:40:26 -0300

    criando os turnos e os jogadores

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,13 +8,23 @@ import chess.pieces.Torre;
 
 public class ChessMatch {
 	
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
 	
+	public int getTurn() {
+		return turn;
+	}
+	public Color getCurrentPlayer() {
+		return currentPlayer;
+	}
 	
 	public ChessPiece[][] getPieces() {
 		
@@ -41,6 +51,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPsition(source,target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -55,6 +66,9 @@ public class ChessMatch {
 		if(!board.thereisAPiece(position)) {
 			throw new ChessException("nao tem peca na posicao");
 		}
+		if(currentPlayer != ((ChessPiece) board.piece(position)).getColor()) {
+			throw new ChessException("peca do adversario"); 
+		}
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("nao tem jogadas possiveis ");
 			
@@ -65,6 +79,11 @@ public class ChessMatch {
 		if(!board.piece(source).possibleMove(target)) {
 			throw new ChessException("a peca nao pode ser movida para esta posicao");
 		}
+	}
+	
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 	
 	private void placeNewPiece(char column , int row, ChessPiece piece) {
